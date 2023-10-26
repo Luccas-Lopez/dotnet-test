0b9f89e002a9031fdddb08c7e05347575ef3775e
Luccas-Lopez <luccaslopes.n@gmail.com>
2023-10-26 13:58:51 -0300

    fix: nova migration para permitir que um produto seja cadastrado sem categorias e vice-versa

---
+++ NycBankDotnetTest/NycBankDotnetTest/Migrations/20231026163653_ProdutosCategoriasNulo.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NycBankDotnetTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProdutosCategoriasNulo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CategoriaProduto",
+                table: "CategoriaProduto");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriaProdutoId",
+                table: "CategoriaProduto",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CategoriaProduto",
+                table: "CategoriaProduto",
+                column: "CategoriaProdutoId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriasId",
+                table: "CategoriaProduto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProdutosId",
+                table: "CategoriaProduto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
